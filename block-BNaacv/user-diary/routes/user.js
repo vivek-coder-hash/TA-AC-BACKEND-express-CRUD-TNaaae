32ed881ce94955ffec634238f95db8ed764ce032
vivek sharma <vivekslim@gmail.com>
2021-07-21 13:35:08 +0530

    CRUD

--- block-BNaacv/user-diary/routes/user.js
+++ block-BNaacv/user-diary/routes/user.js
@@ -2,4 +2,38 @@ var express = require("express")
 var router = express.Router()
 
 
-router.get
+router.get("/" , (req,res)=> {
+    //handle action
+    res.render("user.ejs")
+})
+
+router.post("/" , (req,res)=> {
+    //capture data
+})
+
+router.get("/:id" , (req,res)=> {
+    //single user detail
+})
+
+
+router.get("/new" , (req,res)=> {
+    //render create form
+    res.render("userForm.ejs")
+})
+
+
+router.get("/:id/edit" , (req,res)=> {
+    //edit form
+})
+
+router.put("/:id" , (req,res)=> {
+    // capture updated form
+})
+
+
+router.delete("/:id" , (req,res)=> {
+    //delete user
+})
+
+
+module.exports=router
