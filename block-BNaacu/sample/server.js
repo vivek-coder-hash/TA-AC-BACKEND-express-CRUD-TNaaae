70813457479d79a4f322b40f807e653676b299a0
vivek sharma <vivekslim@gmail.com>
2021-07-21 11:43:15 +0530

    students

---
+++ block-BNaacu/sample/server.js
@@ -0,0 +1,36 @@
+var express = require("express")
+var logger = require("morgan")
+var mongoose = require("mongoose")
+var path = require("path")
+
+
+mongoose.connect("mongodb://localhost/sample" , { useNewUrlParser: true , useUnifiedTopology:true } , (err)=> {
+    console.log(err ? err : "connect to database")
+})
+
+//instantiate app
+
+var app = express()
+
+//middlewares 
+app.use(express.json())
+
+//Routing middlewares
+app.use("/students" , require("./routes/students"))
+
+
+//setup view engine middleware
+app.set("view enjine" , "ejs")
+app.set("views" , path.join(__dirname+"/views"))
+
+
+//Error handler middleware
+app.use((err ,req,res,next)=> {
+    res.send(err)
+})
+
+
+//listener
+app.listen(4000 , ()=> {
+    console.log("server is listening to port 4000")
+})
